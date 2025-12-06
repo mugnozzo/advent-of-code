fa63d8addae316cdb3980f8940f939eff7c065cb
Alek Mugnozzo <mugnozzo@gmail.com>
2025-12-06 15:02:45 +0100

    2025 - day 01 - part 1 - revision

--- 2025/01/python/aoc-2025-01-part1.py
+++ 2025/01/python/aoc-2025-01-part1.py
@@ -6,9 +6,12 @@ c = 0
 with open("../input", "r") as f:
     for l in f:
         l = l.strip()
-        op = 1 if l[0]=="R" else -1
-        n = (n + int(l[1:]) * op) % 100
+        r = int(l[1:])
+        if l[0]=="R":
+            r *= -1
+        n = (n + r) % 100
         if n==0:
             c += 1
+        print(f"{l} - {r} - {n} - {c}")
 
 print(c)
